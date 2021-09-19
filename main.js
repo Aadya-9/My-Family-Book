3ce2d249cb8ad870db365d3524423d686e359178
Aadya-9 <82094543+Aadya-9@users.noreply.github.com>
2021-09-19 15:59:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","papa.jpeg", "mumma.jpeg" , "me.jpeg"];
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","Papa.jpeg", "mumma.jpeg" , "me.jpeg"];
 var names = ["Family Book","Ajoy Agarwal", "Vinita Agarwal", "Aadya Agarwal"];
 var i = 0;
-function update(){
-  var i = 0;
-  i++;
-  var numbers_of_family_member_in_array = 5
-  if(i > numbers_of_family_member_in_array)
+function next_image(){
+  
+ i++
+ var number_of_family_member = 4
+ if(i > number_of_family_member)
+ {i= 0}
       //Debug the code to store list of images in updatedImage. Use images[i]
-  var updatedImage = [images];
+  var updatedImage = images[i];
   //Debug the code to store list of names in updatedName. Use names[i]
-  var updatedName = [names] ;
+  var updatedName = names[i] ;
 
   document.getElementById("family_member_image").src = updatedImage;
   document.getElementById("family_member_name").innerHTML = updatedName;
